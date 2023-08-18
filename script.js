f718f67cd3c47f2608f390609f31c5962ad8c895
Issam Abou <113906543+Issam7Abou@users.noreply.github.com>
2023-08-18 17:36:55 +0100

    Add files via upload
    - fixed some bugs;
    - added the functionality to search by pressing Enter in the userInput;
    - func to get the most popular movies by year;

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// TEST
-const api = 'http://www.omdbapi.com/?apikey=&s=popular';
+//////////////////// TEST  /////// TEST  /////// TEST  /////// TEST
+const api = 'http://www.omdbapi.com/?apikey=4f8f00d1&s=movie&type=movie&y=2022';
 const popularMovies = async function(arg) {
   const response2 = await fetch(arg);
   const data2 = await response2.json();
-  console.log(data2.Search[0]);
+  console.log(data2.Search);
 }
+//////////////////// TEST  /////// TEST  /////// TEST  /////// TEST
 
-popularMovies(api);
-
-// TEST
-
-const apiUrl = 'http://www.omdbapi.com/?apikey=&t=';
+const apiUrl = 'http://www.omdbapi.com/?apikey=4f8f00d1&t=';
 let div = document.querySelector('.search-container');
 let div2 = document.querySelector('.showResult');
 
 // func that is lanched when the user searches for movie
 function showMovie() {
   const userInput = document.getElementById('userInput').value;
-  // Hide Search UI
-  div.style.display = 'none';
-  div2.style.display = 'flex';
-  // Call fetchData to get movie from API
+  if (userInput.length >= 3) {
+    // Hide Search UI & Show Result UI
+    div.style.display = 'none';
+    div2.style.display = 'flex';
+    // Call fetchData to get movie from API
   fetchData(userInput)
+  }
 }
 
 //fetch data from api and search the movie by the User
@@ -47,9 +46,17 @@ function buildUI(data) {
   sinopse.textContent = data.Plot;
 }
 
+//function that fires the showMovie() when the key 'Enter' is pressed in the userInput
+function handleKeyPress(event) {
+  const userInput = document.getElementById('userInput').value;
+  event.keyCode === 13 && userInput.length >= 3 ? showMovie() : null;
+}
+
+// Returns to Home Page from the result movie/serie
 function returnBtn() {
   div2.style.display = 'none';
   div.style.display = 'flex';
 }
 
 //On load
+popularMovies(api);
